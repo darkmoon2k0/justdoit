c07be2d3c3c643943a3e782500fe25df181223ef
han2k <han0944425230@gmail.com>
2025-10-13 09:23:15 +0700

    feat: prepare to deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,16 +3,27 @@ import router from "./routes/tasksRouters";
 import {connectDB} from "./db/db";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from 'path';
 
 dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
+const __dirname = path.resolve();
 
 // middlewares
 app.use(express.json());
 app.use(cors({origin: "http://localhost:3333"}));
 
 app.use("/api/tasks", router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
+}
+
 
 connectDB().then(() => {
   app.listen(PORT, () => {
